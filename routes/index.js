669f8b3e65f775e30302ca9d953be8281c913518
unknown <cesyrub@gmail.com>
2022-05-10 15:59:48 +0200

    Iteration 0

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+const celebritiesRoutes = require("./routes/celebrities.routes.js");
+router.use("/celebrities/", celebritiesRoutes);
+
+const moviesRoutes = require("./routes/movies.routes");
+router.use("/movies/", moviesRoutes);
+
 module.exports = router;
