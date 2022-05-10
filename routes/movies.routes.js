e2841a0b735989b9a16e77a5372dcb174fb7855b
Ana Belen Jover Ballester <anab.jover@gmail.com>
2022-05-10 17:52:05 +0200

    almost done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -26,4 +26,33 @@ router.post("/create", (req, res, next) => {
     });
 });
 
+// LIST GET
+router.get("/", (req, res, next) => {
+
+  MoviesModel.find().select("title")
+  .then((response) => {
+    res.render("movies/movies.hbs", {
+      movies: response
+    })
+  }).catch((err) => {
+    next(err)
+  })
+})
+
+// GET "/movies/:id/"
+router.get("/:id", (req, res, next) => {
+  const {id} = req.params
+
+  MoviesModel.findById(id).populate("cast")
+  .then((response) => {
+    res.render("movies/movie-details.hbs", {
+      detailsMovie: response
+    })
+  }).catch((err) => {
+    next(err)
+  })
+})
+
+
+
 module.exports = router;
